98d9f9bd23d9395113b7e4c075a2045166a70f49
Zihan_Cao <c.zihan@duke.edu>
2024-04-29 16:58:52 -0400

    deployed on kubernetes

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         port: 8081,
         proxy: {
             "^/api": {
-                target: "http://127.0.0.1:3000"
+                target: "http://localhost:3000"
             },
         }
     },
